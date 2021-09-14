f01927c37aed18bc6cf8bf903ce363c74946defd
GianniCarlini <gianni.carlini@sansano.usm.cl>
2021-09-14 17:01:25 -0300

    cambios video section

--- assets/js/main.js
+++ assets/js/main.js
@@ -264,3 +264,17 @@
   });
 
 })()
+var videoBuild = new YoutubeOverlayModule({
+  sourceUrl: 'https://www.youtube.com/embed/1Q8fG0TtVAY',
+  triggerElement: "#demo",
+  progressCallback: function() {
+    console.log("Callback Invoked.");
+  }
+});
+var videoBuild = new YoutubeOverlayModule({
+  sourceUrl: 'https://www.youtube.com/embed/1Q8fG0TtVAY',
+  triggerElement: "#demo",
+  progressCallback: function() {
+    console.log("Callback Invoked.");
+  }
+});
